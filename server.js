53117654e917d15f4feb921cbebbaeb1db34eb32
pstrawberrie <github_main@pstrawberrie.live>
2022-11-05 22:43:23 -0500

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const { JsonDB } = require("node-json-db");
 const { Config } = require("node-json-db/dist/lib/JsonDBConfig");
 
@@ -7,6 +8,7 @@ const app = express();
 const db = new JsonDB(new Config("./data/strawstatic", true, false, "/"));
 
 // Middlewares
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("client"));
